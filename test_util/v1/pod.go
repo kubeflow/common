bc0dc7520018e73a06fbe30ef34c78c25650a6ae
Ce Gao <gaoce@caicloud.io>
2019-04-24 17:24:47 +0800

    chore: Fix package name
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- test_util/v1/pod.go
+++ test_util/v1/pod.go
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package test_util
+package v1
 
 import (
 	"fmt"
 	"testing"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 
