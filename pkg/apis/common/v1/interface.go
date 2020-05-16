4d01e683e1b3671ef5e189499dc73ce17a19a7fc
Jiaxin Shan <seedjeffwan@gmail.com>
2020-05-16 16:01:36 -0700

    Provides more flexibility for customizations (#91)
    * Provides flexibility to customize reconcile logic
    
    1. Move ReconcileJobs, ReconcilePods, ReconcileServices to interface. JobController provides common implementation. User doesn’t necessarily implement these methods a
    nd only user facing change is user need to embed JobController. If operators has custom logic, they can override these reconciling logic
    
    2. Expose few private methods and when user want to have some customization, they can reuse those method
    - DeletePodsAndServices
    - PastBackoffLimit
    - PastActiveDeadline
    - CleanupJob
    
    * Expose methods for kubebuilder based operator
    
    1. JobController has default implementation for GetPodsForJob and GetServicesForJob. User needs to pass a PodLister and SeviceLister. For operators using informers, user don’t need to make any change.
    
    2. However, the challenge part if we have more operators are built with kubebuilder which use higher level APIs and doesn’t create `SharedInformerFactory`. In order to accomendate these cases, we expose methods and user can implement them using latest  reconciler.Client.List().
    
    Signed-off-by: Jiaxin Shan <seedjeffwan@gmail.com>

--- pkg/apis/common/v1/interface.go
+++ pkg/apis/common/v1/interface.go
@@ -26,6 +26,14 @@ type ControllerInterface interface {
 	// Returns the Job from API server
 	GetJobFromAPIClient(namespace, name string) (metav1.Object, error)
 
+	// GetPodsForJob returns the pods managed by the job. This can be achieved by selecting pods using label key "job-name"
+	// i.e. all pods created by the job will come with label "job-name" = <this_job_name>
+	GetPodsForJob(job interface{}) ([]*v1.Pod, error)
+
+	// GetServicesForJob returns the services managed by the job. This can be achieved by selecting services using label key "job-name"
+	// i.e. all services created by the job will come with label "job-name" = <this_job_name>
+	GetServicesForJob(job interface{}) ([]*v1.Service, error)
+
 	// DeleteJob deletes the job
 	DeleteJob(job interface{}) error
 
@@ -47,4 +55,19 @@ type ControllerInterface interface {
 	// Returns if this replica type with index specified is a master role.
 	// MasterRole pod will have "job-role=master" set in its label
 	IsMasterRole(replicas map[ReplicaType]*ReplicaSpec, rtype ReplicaType, index int) bool
+
+	// ReconcileJobs checks and updates replicas for each given ReplicaSpec of a job.
+	// Common implementation will be provided and User can still override this to implement their own reconcile logic
+	ReconcileJobs(job interface{}, replicas map[ReplicaType]*ReplicaSpec, jobStatus JobStatus, runPolicy *RunPolicy) error
+
+	// ReconcilePods checks and updates pods for each given ReplicaSpec.
+	// It will requeue the job in case of an error while creating/deleting pods.
+	// Common implementation will be provided and User can still override this to implement their own reconcile logic
+	ReconcilePods(job interface{}, jobStatus *JobStatus, pods []*v1.Pod, rtype ReplicaType, spec *ReplicaSpec,
+		replicas map[ReplicaType]*ReplicaSpec) error
+
+	// ReconcileServices checks and updates services for each given ReplicaSpec.
+	// It will requeue the job in case of an error while creating/deleting services.
+	// Common implementation will be provided and User can still override this to implement their own reconcile logic
+	ReconcileServices(job metav1.Object, services []*v1.Service, rtype ReplicaType, spec *ReplicaSpec) error
 }
