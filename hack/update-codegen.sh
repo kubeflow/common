b5b18e6930c215a97c0724f5fe67d433af1bfb7f
Johnu George <johnu.george@nutanix.com>
2022-06-04 02:01:27 +0530

    Update all k8s dependencies to 0.24.1 (#194)
    * Update all k8s dependencies to 0.24.1
    
    * Update go flags
    
    * Update go flags
    
    * Fix lint
    
    * Fix mod
    
    * Fix deps

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -22,14 +22,14 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+echo ">> Script root ${SCRIPT_ROOT}"
 ROOT_PKG=github.com/kubeflow/common
 
-# Grab code-generator version from go.sum
-CODEGEN_VERSION=$(grep 'k8s.io/code-generator' go.sum | awk '{print $2}' | head -1)
+# Grab code-generator version from go.mod
+CODEGEN_VERSION=$(grep 'k8s.io/code-generator' go.mod | awk '{print $2}' | head -1)
 CODEGEN_PKG=$(echo `go env GOPATH`"/pkg/mod/k8s.io/code-generator@${CODEGEN_VERSION}")
 
-# Grab kube-openapi version from go.sum
-# Due to dependency issue, we use go.mod to pin right version
+# Grab kube-openapi version from go.mod
 OPENAPI_VERSION=$(grep 'k8s.io/kube-openapi' go.mod | awk '{print $2}' | head -1)
 # remove /go.mod if it happens to match the version
 if [[ $OPENAPI_VERSION == */go.mod ]]; then
@@ -44,7 +44,7 @@ if [[ ! -d ${CODEGEN_PKG} || ! -d ${OPENAPI_PKG} ]]; then
 fi
 
 echo ">> Using ${CODEGEN_PKG}"
-
+echo ">> Using ${OPENAPI_PKG}"
 # Ensure we can execute shell scripts.
 chmod +x ${CODEGEN_PKG}/generate-groups.sh
 
@@ -65,7 +65,7 @@ echo ">> Temporary output directory ${TEMP_DIR}"
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-
+cd ${SCRIPT_ROOT}
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
  github.com/kubeflow/common/pkg/client github.com/kubeflow/common/pkg/apis \
  common:v1 \
@@ -80,34 +80,38 @@ ${CODEGEN_PKG}/generate-groups.sh "all" \
 
 # Notice: The code in code-generator does not generate defaulter by default.
 # We need to build binary from vendor cmd folder.
-echo "Building defaulter-gen"
-go get k8s.io/code-generator/cmd/defaulter-gen@v0.19.9
-go build -o ${GOPATH}/bin/defaulter-gen ${CODEGEN_PKG}/cmd/defaulter-gen
+#echo "Building defaulter-gen"
+#go get k8s.io/code-generator/cmd/defaulter-gen@v0.19.9
+#go build -o ${GOPATH}/bin/defaulter-gen ${CODEGEN_PKG}/cmd/defaulter-gen
 
 echo "Generating defaulters for common/v1"
 ${GOPATH}/bin/defaulter-gen --input-dirs github.com/kubeflow/common/pkg/apis/common/v1 \
 -O zz_generated.defaults \
---output-base "${TEMP_DIR}" \
---go-header-file hack/boilerplate/boilerplate.go.txt
+--output-package github.com/kubeflow/common/pkg/apis/common/v1 \
+--go-header-file hack/boilerplate/boilerplate.go.txt "$@" \
+--output-base "${TEMP_DIR}" 
 
 echo "Generating defaulters for test_job/v1"
 ${GOPATH}/bin/defaulter-gen --input-dirs github.com/kubeflow/common/test_job/apis/test_job/v1 \
 -O zz_generated.defaults \
---output-base "${TEMP_DIR}" \
---go-header-file hack/boilerplate/boilerplate.go.txt
+--output-package github.com/kubeflow/common/test_job/apis/test_job/v1 \
+--go-header-file hack/boilerplate/boilerplate.go.txt "$@" \
+--output-base "${TEMP_DIR}" 
 
 echo "Building openapi-gen"
-go build -o ${GOPATH}/bin/openapi-gen ${OPENAPI_PKG}/cmd/openapi-gen
+GOFLAGS=-mod=mod go build -o ${GOPATH}/bin/openapi-gen ${OPENAPI_PKG}/cmd/openapi-gen
 
 echo "Generating OpenAPI specification for common/v1"
-${GOPATH}/bin/openapi-gen --input-dirs github.com/kubeflow/common/pkg/apis/common/v1,k8s.io/api/core/v1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/util/intstr,k8s.io/apimachinery/pkg/version \
+${GOPATH}/bin/openapi-gen --input-dirs github.com/kubeflow/common/pkg/apis/common/v1 \
 --output-package github.com/kubeflow/common/pkg/apis/common/v1 \
---go-header-file hack/boilerplate/boilerplate.go.txt
+--go-header-file hack/boilerplate/boilerplate.go.txt "$@" \
+--output-base "${TEMP_DIR}" 
 
 echo "Generating OpenAPI specification for test_job/v1"
-${GOPATH}/bin/openapi-gen --input-dirs github.com/kubeflow/common/test_job/apis/test_job/v1,k8s.io/api/core/v1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/util/intstr,k8s.io/apimachinery/pkg/version \
+${GOPATH}/bin/openapi-gen --input-dirs github.com/kubeflow/common/test_job/apis/test_job/v1 \
 --output-package github.com/kubeflow/common/test_job/apis/test_job/v1 \
---go-header-file hack/boilerplate/boilerplate.go.txt
+--go-header-file hack/boilerplate/boilerplate.go.txt "$@" \
+--output-base "${TEMP_DIR}" 
 
 ## Copy everything back.
 cp -a "${TEMP_DIR}/${ROOT_PKG}/." "${SCRIPT_ROOT}/"
