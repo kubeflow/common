a36cac65db1c03662025b4b6cbdbae806a664ae9
yowenter <wenter.wu@gmail.com>
2023-04-21 16:57:39 +0800

    fix job active count;  count terminating pod as failed
    Signed-off-by: yowenter <wenter.wu@gmail.com>

--- pkg/core/status.go
+++ pkg/core/status.go
@@ -34,7 +34,14 @@ func InitializeReplicaStatuses(jobStatus *apiv1.JobStatus, rtype apiv1.ReplicaTy
 func UpdateJobReplicaStatuses(jobStatus *apiv1.JobStatus, rtype apiv1.ReplicaType, pod *corev1.Pod) {
 	switch pod.Status.Phase {
 	case corev1.PodRunning:
-		jobStatus.ReplicaStatuses[rtype].Active++
+		if pod.DeletionTimestamp != nil {
+			// when node is not ready, the pod will be in terminating state.
+			// Count deleted Pods as failures to account for orphan Pods that
+			// never have a chance to reach the Failed phase.
+			jobStatus.ReplicaStatuses[rtype].Failed++
+		} else {
+			jobStatus.ReplicaStatuses[rtype].Active++
+		}
 	case corev1.PodSucceeded:
 		jobStatus.ReplicaStatuses[rtype].Succeeded++
 	case corev1.PodFailed:
